8a9642f887e1a99a1259ddddfe6c472311687c80
mainyh <mainyh@gmail.com>
2023-02-09 18:12:09 +0900

    회사에서 진행 한 스터디

--- test.py
+++ test.py
@@ -135,29 +135,110 @@ python = "Python is Amazing"
 # print(cabinet.get(1))
 # print(cabinet.get(10))
 
-def open_account():
-    print("계좌가 생성되었습니다.")
+# def open_account():
+#     print("계좌가 생성되었습니다.")
     
-open_account()
+# open_account()
 
-def deposit(balance, money):
-    print("입금이 완료 되었습니다. 잔액은 {0}원 입니다.".format(balance + money))
-    return balance + money
+# def deposit(balance, money):
+#     print("입금이 완료 되었습니다. 잔액은 {0}원 입니다.".format(balance + money))
+#     return balance + money
 
-def withdraw(balance, money):
-    if balance >= money:
-        print("출금이 완료 되었습니다. 잔액은 {0}원 입니다.".format(balance - money))
-        return balance - money
-    else:
-        print("잔액이 부족합니다. 잔액은 {0}원 입니다.".format(balance))
-        return balance
+# def withdraw(balance, money):
+#     if balance >= money:
+#         print("출금이 완료 되었습니다. 잔액은 {0}원 입니다.".format(balance - money))
+#         return balance - money
+#     else:
+#         print("잔액이 부족합니다. 잔액은 {0}원 입니다.".format(balance))
+#         return balance
+
+# balance = 0
+# balance = deposit(balance, 1000)
+# print(balance)
+
+# balance = withdraw(balance, 2000)
+# balance = withdraw(balance, 500)
+# print(balance)
+
+# def withdraw_night(balance, money):
+#     commission = 100
+#     return commission, balance - money - commission
+
+# balance = 0
+# balance = deposit(balance, 1000)
+
+# commission, balance = withdraw_night(balance, 500)
+# print("수수료 {0} 원이며, 잔액은 {1} 원입니다.".format(commission, balance))
+
+# def profile(name, age=17, main_lang="파이썬"):
+#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
+
+# profile("유재석")
+# profile("김태호", 20)
+# profile("김태호", 20, "자바")
+
+# def profile(name, age, main_lang): # 키워드 인자 : name, age, main_lang
+#     print(name, age, main_lang)
+
+# # profile("유재석", 20, "파이썬")
+# # profile("김태호", 25, "자바")
+
+# profile(name="유재석", main_lang="파이썬", age=20)
+# profile(main_lang="자바", age=25, name="김태호")
+# profile(name="김종국", age=30, main_lang="자바")
+
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end="")
+#     print(lang1, lang2, lang3, lang4, lang5)
+    
+# profile("유재석", 20, "Python", "Java", "C", "C++", "C#")
+# profile("김태호", 25, "Kotlin", "Swift", "", "", "")
+
+# def profile(name, age, *language):
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+    
+#     for lang in language:
+#         print(lang, end=" ")
+#     print()    
+    
+# profile("유재석", 25, "Python", "Java", "C", "C++", "C#", "Kotlin")
+# profile("김태호", 20, "Kotlin")
+
+# gun = 10
+
+# def checkpoint(soldiers):
+#     global gun
+#     gun = gun - soldiers
+#     print("[함수 내] 남은 총 : {0}".format(gun))
+    
+# print("전체 총 : {0}".format(gun))
+# checkpoint(2)
+# print("남은 총 : {0}".format(gun))
+
+# def checkpoint_ret(gun, soldiers):
+#     gun = gun - soldiers
+#     print("[함수 내] 남은 총 : {0}".format(gun))
+#     return gun
+
+# print("전체 총 : {0}".format(gun))
+# gun = checkpoint_ret(gun, 2)
+# print("남은 총 : {0}".format(gun))
+
+def std_weight(height, gender):
+    if gender == "남자":
+        std_weight = height * height * 22
+        return(std_weight)
+    elif gender == "여자":
+            return height * height *21
+            
+
+height = 175
+gender = "여자"            
+weight = round(std_weight(height / 100, gender),2)
+print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
+            
+            
+            
+            
         
-
-
-balance = 0
-balance = deposit(balance, 1000)
-print(balance)
-
-balance = withdraw(balance, 2000)
-balance = withdraw(balance, 500)
-print(balance)
+    
